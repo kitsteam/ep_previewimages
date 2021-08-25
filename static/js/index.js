14d23ef58d190f10bf67276fb54bf0044d8383c8
tjark002 <tjarkh@arcor.de>
2021-08-25 16:27:14 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -5,13 +5,19 @@ exports.aceGetFilterStack = function (name, context) {
   ];
 };
 
-
 exports.aceCreateDomLine = function (name, args) {
+  
   if (args.cls.indexOf('image') > -1) { // If it's an image
+    console.log("aceCreateDomLine called");
     let src;
     cls = args.cls.replace(/(^| )image:(\S+)/g, (x0, space, image) => {
-      //src = "../html/invalid.png";
-      src = "../ep_previewimages/static/html/invalid.png";
+      src = image;
+      if (!imageExists(src)) {
+          console.log("Wir landen richtig");
+          src = "https://raw.githubusercontent.com/tjark002/ep_previewimages/master/static/html/invalid.png";
+      } else {
+          console.log("Von mir aus!");
+      }
       return `${space}image image_${image}`;
     });
 
@@ -19,9 +25,20 @@ exports.aceCreateDomLine = function (name, args) {
     // Note the additional span wrapper here is required to stop errors if someone types text after an image url
     return [{
       cls,
-      //extraOpenTags: `<span style="display:block;"><img src="${src}" onerror="this.src='https://visionz.de/wp-content/uploads/2021/03/01_Visionz_web-scaled-e1616685650690.jpg';" style="max-width:100%" /></span>`,
+      //extraOpenTags: `<span style="display:block;"><img src="${src}" onerror="this.src='https://raw.githubusercontent.com/tjark002/ep_previewimages/master/static/html/invalid.png';" style="max-width:100%" /></span>`,
       extraOpenTags: `<span style="display:block;"><img src="${src}" style="max-width:100%" /></span>`,
       extraCloseTags: '',
     }];
   }
 };
+
+function imageExists(image_url){
+
+    var http = new XMLHttpRequest();
+
+    http.open('HEAD', image_url, false);
+    http.send();
+
+    return http.status != 404;
+
+}

f864904c1eb306aa3f9fd0336a8a4259158fdcf2
tjark002 <tjarkh@arcor.de>
2021-08-25 11:24:31 +0200

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,8 @@ exports.aceCreateDomLine = function (name, args) {
   if (args.cls.indexOf('image') > -1) { // If it's an image
     let src;
     cls = args.cls.replace(/(^| )image:(\S+)/g, (x0, space, image) => {
-      src = image;
+      //src = "../html/invalid.png";
+      src = "ep_previewimages/static/html/invalid.png";
       return `${space}image image_${image}`;
     });
 
@@ -18,6 +19,7 @@ exports.aceCreateDomLine = function (name, args) {
     // Note the additional span wrapper here is required to stop errors if someone types text after an image url
     return [{
       cls,
+      //extraOpenTags: `<span style="display:block;"><img src="${src}" onerror="this.src='https://visionz.de/wp-content/uploads/2021/03/01_Visionz_web-scaled-e1616685650690.jpg';" style="max-width:100%" /></span>`,
       extraOpenTags: `<span style="display:block;"><img src="${src}" style="max-width:100%" /></span>`,
       extraCloseTags: '',
     }];
